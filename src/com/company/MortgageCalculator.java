e50f272d58e822d077e107d4c5827a599d921888
danieltanyi Github <tanyi_daniel@yahoo.com>
2022-06-12 15:44:55 +0200

    Encapsulating all methods

--- src/com/company/MortgageCalculator.java
+++ src/com/company/MortgageCalculator.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public class MortgageCalculator {
-    public final static byte MONTHS_IN_YEAR = 12;
-    public final static byte PERCENT = 100;
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
     // lets introduce the encapsulation principles
     private int principal;
     private float annualInterest;
@@ -18,8 +18,8 @@ public class MortgageCalculator {
     }
 
     public  double calculateBalance(short numberOfPaymentsMade) {
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        float numberOfPayments = years * MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -28,10 +28,12 @@ public class MortgageCalculator {
         return balance;
     }
 
+
+
     public  double calculateMortgage() {
 
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        float numberOfPayments = years * MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
@@ -41,7 +43,19 @@ public class MortgageCalculator {
 
     }
 
-    public short getYears() {
-        return years;
+    public double[] getRemainingBalances() {
+        var balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= balances.length; month++)
+            balances [month-1] = calculateBalance(month);
+
+        return balances;
+    }
+
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
+    }
+
+    private int getNumberOfPayments() {
+        return years * MONTHS_IN_YEAR;
     }
 }
